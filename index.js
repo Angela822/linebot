f69f56fdc92b3bafa3e3ebbd68400e3ce36fdb56
takebook <takebook107511@gmail.com>
2018-08-11 21:56:17 +0800

    reply

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const linebot = require('linebot');
 const express = require('express');
+const line = require('@line/bot-sdk');
 
 const bot = linebot({
 	channelId: process.env.CHANNEL_ID,
 	channelSecret: process.env.CHANNEL_SECRET,
 	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 });
+
+const client = new line.Client({
+	channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+  });
 
 const app = express();
 
@@ -17,13 +22,31 @@ app.get('/',function(req,res){
 
 app.post('/linewebhook', linebotParser);
 
+const message = {
+	type: 'text',
+	text: 'Hello World!'
+};
+
+reply_token = params['events'][0]['replyToken']
+
+client.replyMessage(reply_token, message)
+  .then(() => {
+    console.log('Success');
+})
+  .catch((err) => {
+    console.log('Error', error);
+  });
+
+/*
+//--reply the same message--
 bot.on('message', function (event) {
 	event.reply(event.message.text).then(function (data) {
 		console.log('Success', data);
 	}).catch(function (error) {
 		console.log('Error', error);
 	});
-});
+});*/
+//--------------------------
 
 app.listen(process.env.PORT || 80, function () {
 	console.log('LineBot is running.');

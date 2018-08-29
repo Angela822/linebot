12a3f0f0154f9854ac0029088f9f94479b7b2aa6
takebook <takebook107511@gmail.com>
2018-08-29 15:38:37 +0800

    add received_text

--- index.js
+++ index.js
@@ -383,7 +383,7 @@ function handleEvent(event) {
     return client.replyMessage(event.replyToken, require);
   }else if(event.message.text == '用類別找書'){
     return client.replyMessage(event.replyToken, typeBook);
-  }else if(received_text.substring(1,2) == '文'){
+  }else if(received_text.substring(0,1) == '文'){
     return client.replyMessage(event.replyToken, test);
   }
   //-------------------------------

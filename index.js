f02fe64f9d50fcaffe210aef2f33d3910ed6a692
takebook <takebook107511@gmail.com>
2018-08-31 20:33:56 +0800

    typebook

--- index.js
+++ index.js
@@ -382,8 +382,6 @@ function handleEvent(event) {
     return client.replyMessage(event.replyToken, newBook);
   }else if(received_text == '查詢'){
     return client.replyMessage(event.replyToken, require);
-  }else if(received_text == '用類別找書'){
-    return client.replyMessage(event.replyToken, typeBook);
   }else if(received_text.substring(0,1) == '文'){
     return client.replyMessage(event.replyToken, test);
   }

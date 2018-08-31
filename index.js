4de881dba4e835ed4a676e3da57bc65a20730653
takebook <takebook107511@gmail.com>
2018-08-31 21:11:16 +0800

    typebook

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ function handleEvent(event) {
   //用類別找書
   const typeBook = {
     type: 'text',
-    text: '我想看:XX,XX,XX (Ex.我想看:文學,生活風格,藝術設計)'
+    text: '我想看：XX,XX,XX (Ex.我想看：文學,生活風格,藝術設計)'
   }
 
   //新書推薦
@@ -357,7 +357,7 @@ function handleEvent(event) {
     return client.replyMessage(event.replyToken, require);
   }else if(received_text == '用類別找書'){
     return client.replyMessage(event.replyToken, typeBook);
-  }else if(received_text.substring(0,4) == '我想看:'){
+  }else if(received_text.substring(0,4) == '我想看：'){
     return client.replyMessage(event.replyToken, test);
   }
   //-------------------------------

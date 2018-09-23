c04f82f2833fbc28d0b018cfd02bd1850bbf59e9
takebook <takebook107511@gmail.com>
2018-09-23 17:38:49 +0800

    change format

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 'use strict';
 
-//--------------------------------
-// 載入必要的模組
-//--------------------------------
-var linebot = require('linebot');
-var express = require('express');
+const line = require('@line/bot-sdk');
+const express = require('express');
 
-//--------------------------------
-// 填入自己在linebot的channel值
-//--------------------------------
-var bot = linebot({
-  channelId: '1593876483',
-  channelSecret: '01ac1bb252593c63605da3942b9baabc',
-  channelAccessToken: 'g93gFjGS2nxtZtwdGYwFg2Sd+i7eO7C1imlK96heyVGV76dLwRPXO1qseNi4R7poSpv3P1KnNsQle4MStyTrTgd8O2eGK+6yUnJkTELfeQPp1y9hj/MB+S03z99VpKL3IO8JUbuS2G7jRwJ8WqmKSgdB04t89/1O/w1cDnyilFU='
-});
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
 // create LINE SDK client
-const client = new linebot.Client(config);
+const client = new line.Client(config);
 
-//--------------------------------
-// 建立一個網站應用程式app
-// 如果連接根目錄, 交給機器人處理
-//--------------------------------
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
 app.post('/linewebhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
@@ -33,34 +28,12 @@ app.post('/linewebhook', line.middleware(config), (req, res) => {
     });
 });
 
+//--------------------------------
+// 可直接取用檔案的資料夾
+//--------------------------------
+app.use(express.static('public'));
+
 //--------------Main Start---------------------
-
-//--------------------------
-// 機器人接受回覆的處理
-//--------------------------
-bot.on('postback', function(event) { 
-  var data = event.postback.data;
-  var userId = event.source.userId;
-
-  event.source.profile().then(function (profile) {
-      userName = profile.displayName;
-  
-      return event.reply([
-          {
-              "type": "text",
-              "text": data
-          },
-          {
-              "type": "text",
-              "text": userId
-          },
-          {
-              "type": "text",
-              "text": userName
-          }
-      ]);		
-  });
-});
 
 // event handler
 function handleEvent(event) {
@@ -68,7 +41,7 @@ function handleEvent(event) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
- 
+  
   // create a echoing text message
   const echo = { 
 		"type": 'template', 
@@ -397,7 +370,7 @@ function handleEvent(event) {
   }else if(received_text.substring(0,4) == '我想看：'){
     return client.replyMessage(event.replyToken, typeTrue);
   }
-  // use reply API?
+  // use reply API
   return client.replyMessage(event.replyToken, echo);
   
   /*
@@ -415,22 +388,16 @@ function handleEvent(event) {
   if(bookType == 'bookType=01'){
     return client.replyMessage(event.replyToken, '文學');
   }*/
-    
   
+  
+  
+
 }
 
 //--------------Main End---------------------
 
-//--------------------------------
-// 可直接取用檔案的資料夾
-//--------------------------------
-app.use(express.static('public'));
-
-//--------------------------------
-// 監聽3000埠號, 
-// 或是監聽Heroku設定的埠號
-//--------------------------------
-var server = app.listen(process.env.PORT || 3000, function() {
-  var port = server.address().port;
-  console.log("正在監聽埠號:", port);
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
 });

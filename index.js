9bd0685ec5f12a5efbca16f281b5f264e2da07d6
takebook <takebook107511@gmail.com>
2018-09-24 19:13:08 +0800

    add postback

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(express.static('public'));
 
 //--------------Main Start---------------------
 
-/*
+
 //--------------------------
 // 機器人接受回覆的處理
 //--------------------------
@@ -61,7 +61,7 @@ bot.on('postback', function(event) {
           }
       ]);		
   });
-});*/
+});
 
 // event handler
 function handleEvent(event) {
@@ -69,6 +69,33 @@ function handleEvent(event) {
     // ignore non-text-message event
     return Promise.resolve(null);
   }
+
+  //--------------------------
+  // 機器人接受回覆的處理
+  //--------------------------
+  bot.on('postback', function(event) { 
+    var data = event.postback.data;
+    var userId = event.source.userId;
+
+    event.source.profile().then(function (profile) {
+        userName = profile.displayName;
+    
+        return event.reply([
+            {
+                "type": "text",
+                "text": data
+            },
+            {
+                "type": "text",
+                "text": userId
+            },
+            {
+                "type": "text",
+                "text": userName
+            }
+        ]);		
+    });
+  });
   
   // create a echoing text message
   const echo = { 

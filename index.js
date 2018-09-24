241c7ee01154d6b0112bc8b284591982a9d93a25
takebook <takebook107511@gmail.com>
2018-09-24 19:14:01 +0800

    add postback

--- index.js
+++ index.js
@@ -70,33 +70,7 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  //--------------------------
-  // 機器人接受回覆的處理
-  //--------------------------
-  bot.on('postback', function(event) { 
-    var data = event.postback.data;
-    var userId = event.source.userId;
-
-    event.source.profile().then(function (profile) {
-        userName = profile.displayName;
-    
-        return event.reply([
-            {
-                "type": "text",
-                "text": data
-            },
-            {
-                "type": "text",
-                "text": userId
-            },
-            {
-                "type": "text",
-                "text": userName
-            }
-        ]);		
-    });
-  });
-  
+ 
   // create a echoing text message
   const echo = { 
 		"type": 'template', 

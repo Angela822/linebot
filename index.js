ea60aef4e0ed9d2d14d0c3c1aecee4d261e8916e
takebook <takebook107511@gmail.com>
2018-09-24 19:02:36 +0800

    config-->bot

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 
 // create LINE SDK config from env variables
-const config = {
+/*const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
-};
+};*/
+
+var bot = linebot({
+  channelId: '1593876483',
+  channelSecret: '01ac1bb252593c63605da3942b9baabc',
+  channelAccessToken: 'g93gFjGS2nxtZtwdGYwFg2Sd+i7eO7C1imlK96heyVGV76dLwRPXO1qseNi4R7poSpv3P1KnNsQle4MStyTrTgd8O2eGK+6yUnJkTELfeQPp1y9hj/MB+S03z99VpKL3IO8JUbuS2G7jRwJ8WqmKSgdB04t89/1O/w1cDnyilFU='
+});
 
 // create LINE SDK client
-const client = new line.Client(config);
+const client = new line.Client(bot);
 
 // create Express app
 // about Express itself: https://expressjs.com/
@@ -18,7 +24,7 @@ const app = express();
 
 // register a webhook handler with middleware
 // about the middleware, please refer to doc
-app.post('/linewebhook', line.middleware(config), (req, res) => {
+app.post('/linewebhook', line.middleware(bot), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
@@ -38,7 +44,7 @@ app.use(express.static('public'));
 //--------------------------
 // 機器人接受回覆的處理
 //--------------------------
-config.on('postback', function(event) { 
+bot.on('postback', function(event) { 
   var data = event.postback.data;
   var userId = event.source.userId;
 

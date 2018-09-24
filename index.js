e9ac032fb5857cdf3af9a649e5b485b490913ba0
takebook <takebook107511@gmail.com>
2018-09-24 23:04:41 +0800

    test

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ var bot = linebot({
   channelAccessToken: 'g93gFjGS2nxtZtwdGYwFg2Sd+i7eO7C1imlK96heyVGV76dLwRPXO1qseNi4R7poSpv3P1KnNsQle4MStyTrTgd8O2eGK+6yUnJkTELfeQPp1y9hj/MB+S03z99VpKL3IO8JUbuS2G7jRwJ8WqmKSgdB04t89/1O/w1cDnyilFU='
 });
 
-const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
-};
-
-// create LINE SDK client
-const client = new line.Client(config);
 
 // create Express app
 // about Express itself: https://expressjs.com/
@@ -70,365 +63,42 @@ app.use(express.static('public'));
     });
   });
 
-// event handler
-function handleEvent(event) {
-  if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
-    return Promise.resolve(null);
-  }
-  
-  // create a echoing text message
-  const echo = { 
-		"type": 'template', 
-    //text: '你可以試著打"你會做什麼"'//event.message.text 
-    "altText": "你可以試著打'你會做什麼'",
-    "template": {
-        "type": "buttons",
-        "title": "你可以試著打'你會做什麼'",
-        "text": "請選擇",
-        "actions": [
-            {
-              "type": "message",
-              "label": "你會做什麼",
-              "text": "你會做什麼"
-            }
-        ]
-    }
-  }
-　
-  //收集使用者的喜好
-  const habit = {
-    "type": "template",
-    "altText": "喜歡這本書嗎?",
-    "template": {
-      "type": "confirm",
-      "text": "喜歡這本書嗎?",//提示字，會出現在選項的上面
-      "actions": [
-        {
-          "type": "postback",
-          "label": "Like",
-          "data": "1"
-        },
-        {
-          "type": "postback",
-          "label": "Dislike",
-          "data": "0"
+  //--------------------------------
+// 機器人接受訊息的處理
+//--------------------------------
+bot.on('message', function(event) {
+	event.reply({
+        "type": "template",
+        "altText": "This is a buttons template",
+        "template": {
+            "type": "buttons",
+            "thumbnailImageUrl": "https://test0921107511.herokuapp.com/imgs/1.jpg",
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover",
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "這是什麼花?",
+            "text": "是菊科向日葵屬的植物。別名太陽花。",
+            "defaultAction": {
+                "type": "uri",
+                "label": "檢視網頁",
+                "uri": "https://zh.wikipedia.org/wiki/向日葵"
+            },
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "鳶尾花",
+                    "data": "1"
+                },
+                {
+                    "type": "postback",
+                    "label": "向日葵",
+                    "data": "2"
+                }				
+            ]
         }
-      ]
-    }
-  }
+    });
+});
 
-  //查詢
-  const require = {
-    "type": "template",
-    "altText": "查詢",
-    "template": {
-        "type": "buttons",
-        "text": "查詢",
-        "actions": [
-            {
-              "type": "message",
-              "label": "關鍵字找書",
-              "text": "關鍵字找書"
-            },
-            {
-              "type": "message",
-              "label": "書本排行榜",
-              "text": "書本排行榜"
-            }
-        ]
-    }
-  }
-
-  //找書
-  const findBook = {
-    "type": "template",
-    "altText": "找書",
-    "template": {
-        "type": "buttons",
-        "text": "找書",
-        "actions": [
-            {
-              "type": "message",
-              "label": "用類別找書",
-              "text": "用類別找書"
-            },
-            {
-              "type": "message",
-              "label": "新書",
-              "text": "新書"
-            },
-            {
-              "type": "message",
-              "label": "機器人推薦書本",
-              "text": "機器人推薦書本"
-            }
-        ]
-    }
-  }
-
-  //用類別找書
-  const typeBook = {
-    type: 'text',
-    text: '我想看：XX,XX,XX (Ex.我想看：文學,生活風格,藝術設計)'
-  }
-
-  const typeFalse = {
-    type: 'text',
-    text: '你這樣不對喔，要按照格式打~'
-  }
-
-  const typeTrue = {
-    type: 'text',
-    text: '好的好的~'
-  }
-
-  //新書推薦
-  const newBook = {
-    "type": "template",
-    "altText": "新書推薦",
-    "template": {
-        "type": "carousel",
-        "columns": [
-            {
-              "thumbnailImageUrl": "https://linebot-takebook.herokuapp.com/imgs/我有破壞自己的權利.jpg",
-              "imageAspectRatio": "rectangle",
-              "imageSize": "cover",
-              "imageBackgroundColor": "#FFFFFF",
-              "title": "<<我有破壞自己的權利>>",
-              "text": "類別：文學",
-              "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://140.131.114.176/"
-              },
-              "actions": [
-                  {
-                      "type": "message",
-                      "label": "Like or Dislike?",
-                      "text": "Like or Dislike?"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "看更多...",
-                      "uri": "https://www.books.com.tw/products/0010794069?loc=P_011_0_101"
-                  }
-              ]
-            },
-            {
-              "thumbnailImageUrl": "https://linebot-takebook.herokuapp.com/imgs/甜素烘焙實驗室.jpg",
-              "imageBackgroundColor": "#000000",
-              "title": "<<甜素烘焙實驗室>>",
-              "text": "類別：飲食料理",
-              "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://140.131.114.176/"
-              },
-              "actions": [
-                  {
-                      "type": "message",
-                      "label": "Like or Dislike?",
-                      "text": "Like or Dislike?"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "看更多...",
-                      "uri": "http://www.books.com.tw/products/0010794498?loc=P_016_0_102"
-                  }
-              ]
-            },
-            {
-              "thumbnailImageUrl": "https://linebot-takebook.herokuapp.com/imgs/願你的深情，能被溫柔以待.jpg",
-              "imageBackgroundColor": "#000000",
-              "title": "<<願你的深情，能被溫柔以待>",
-              "text": "類別：心理勵志",
-              "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://140.131.114.176/"
-              },
-              "actions": [
-                  {
-                      "type": "message",
-                      "label": "Like or Dislike?",
-                      "text": "Like or Dislike?"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "看更多...",
-                      "uri": "http://www.books.com.tw/products/0010794010?loc=P_017_005"
-                  }
-              ]
-            }
-        ],
-        "imageAspectRatio": "rectangle",
-        "imageSize": "cover"
-    }
-  }
-
-  //推薦書本
-  const recommend = {
-    "type": "template",
-    "altText": "推薦給您...",
-    "template": {
-        "type": "carousel",
-        "columns": [
-            {
-              "thumbnailImageUrl": "https://linebot-takebook.herokuapp.com/imgs/没問題，我可以搞定.jpg",
-              "imageBackgroundColor": "#FFFFFF",
-              "title": "<<没問題，我可以搞定>>",
-              "text": "類別：生活教育",
-              "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://140.131.114.176/"
-              },
-              "actions": [
-                  {
-                      "type": "message",
-                      "label": "Like or Dislike?",
-                      "text": "Like or Dislike?"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "看更多...",
-                      "uri": "http://www.books.com.tw/products/0010792988?loc=P_002_012"
-                  }
-              ]
-            },
-            {
-              "thumbnailImageUrl": "https://linebot-takebook.herokuapp.com/imgs/讓男人追著妳跑.jpg",
-              "imageBackgroundColor": "#000000",
-              "title": "<<讓男人追著妳跑>>",
-              "text": "類別：心理勵志",
-              "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://140.131.114.176/"
-              },
-              "actions": [
-                  {
-                      "type": "message",
-                      "label": "Like or Dislike?",
-                      "text": "Like or Dislike?"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "看更多...",
-                      "uri": "http://www.books.com.tw/products/0010779366?loc=P_002_001"
-                  }
-              ]
-            },
-            {
-              "thumbnailImageUrl": "https://linebot-takebook.herokuapp.com/imgs/跟著阿滴滴妹說出溜英文.jpg",
-              "imageBackgroundColor": "#000000",
-              "title": "<<跟著阿滴滴妹說出溜英文>",
-              "text": "類別：語言／字辭典",
-              "defaultAction": {
-                  "type": "uri",
-                  "label": "View detail",
-                  "uri": "http://140.131.114.176/"
-              },
-              "actions": [
-                  {
-                      "type": "message",
-                      "label": "Like or Dislike?",
-                      "text": "Like or Dislike?"
-                  },
-                  {
-                      "type": "uri",
-                      "label": "看更多...",
-                      "uri": "http://www.books.com.tw/products/0010790130?loc=P_002_017"
-                  }
-              ]
-            }
-        ],
-        "imageAspectRatio": "rectangle",
-        "imageSize": "cover"
-    }
-  }
-
-  //takebook機器人功能選單
-  const botMenu = {
-    "type": "template",
-    "altText": "我會做這些事...",
-    "template": {
-        "type": "buttons",
-        "title": "我會做這些事",
-        "text": "請選擇",
-        "actions": [
-            {
-              "type": "message",
-              "label": "查詢",
-              "text": "查詢"
-            },
-            {
-              "type": "message",
-              "label": "找書",
-              "text": "找書"
-            },
-            {
-              "type": "uri",
-              "label": "表單分析",
-              "uri":"https://goo.gl/forms/usqTTQ8nyVnqfdqi1" //Google Forms:書本類型取向
-            },
-            {
-              "type": "uri",
-              "label": "瀏覽Take Book網站",
-              "uri": "http://140.131.114.176/"
-            }
-        ]
-    }
-  }
-
-  /*Wrong 
-  const chooseType = [];
-  chooseType.push(event.postback.postback.data);
-  */
-
-  //----------關鍵字回覆---------------
-  var received_text = event.message.text;
-
-  if(received_text == 'Like or Dislike?'){
-    return client.replyMessage(event.replyToken, habit);
-  }else if(received_text == '機器人推薦書本'){
-    return client.replyMessage(event.replyToken, recommend);
-  }else if(received_text == '你會做什麼'){
-    return client.replyMessage(event.replyToken, botMenu);
-  }else if(received_text == '找書'){
-    return client.replyMessage(event.replyToken, findBook);
-  }else if(received_text == '新書'){
-    return client.replyMessage(event.replyToken, newBook);
-  }else if(received_text == '查詢'){
-    return client.replyMessage(event.replyToken, require);
-  }else if(received_text == '用類別找書'){
-    return client.replyMessage(event.replyToken, typeBook);
-  }else if(received_text.substring(0,4) == '我想看：'){
-    return client.replyMessage(event.replyToken, typeTrue);
-  }
-  // use reply API
-  return client.replyMessage(event.replyToken, echo);
-  
-  /*
-  if(received_text.substring(0,4) == '我想看：'){
-    return client.replyMessage(event.replyToken, typeTrue);
-  }else{
-    return client.replyMessage(event.replyToken, typeFalse);
-  }
-  */
-
-
-  //-------------------------------
-  /*
-  const bookType = event.postback.postback.data;
-  if(bookType == 'bookType=01'){
-    return client.replyMessage(event.replyToken, '文學');
-  }*/
-   
-}
-
-//--------------Main End---------------------
 
 // listen on port
 const port = process.env.PORT || 3000;

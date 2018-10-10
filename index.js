fb32e7da1889cdfe8cbc9e2bd6235f7eca3a959f
takebook <takebook107511@gmail.com>
2018-10-10 18:23:43 +0800

    類別-判斷使用者id是否存在

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ bot.on('message',function(event) {
                             //(資料庫欄位名稱不使用駝峰命名, 否則可能出錯)
 
                         client.query("select type from userhabit where userid = $1", [userId], (err, results) =>{
-                            if(err){
+                            if(err || results.rows.length==0){
                                 /*
                                 client.query("insert into userhabit(userid) values($1)", [userId], (err, results) => {    
                                     console.log(results);

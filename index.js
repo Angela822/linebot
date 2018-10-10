abce1e3dcc75ffe34c9161da7ee2ac1d12486007
takebook <takebook107511@gmail.com>
2018-10-10 16:14:43 +0800

    類別-資料收集

--- index.js
+++ index.js
@@ -283,7 +283,7 @@ bot.on('message',function(event) {
                     
                     //查詢資料
                     //(資料庫欄位名稱不使用駝峰命名, 否則可能出錯)
-                        client.query("update userhabit set $1 = 101 where userid = $2", [type,userId], (err, results) => {    
+                        client.query("update userhabit set $1 = '101' where userid = $2", [type,userId], (err, results) => {    
                             console.log(results);
                             
                             //回覆查詢結果

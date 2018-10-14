49d4282a7eb2356f8bfbf8b62ab1591f3f04f34c
takebook <takebook107511@gmail.com>
2018-10-14 17:04:46 +0800

    機器人推薦

--- index.js
+++ index.js
@@ -40,13 +40,12 @@ var bot = linebot({
                 if(event.postback.data.substring(0,3) == '我喜歡'){
                     client.query("update userhabit set count = count + 1 where type = $1 AND userid = $2", [type,userId], (err, results) => {    
                         console.log(results);
-                        var count = results.rows[0].count;
                         
                         //回覆查詢結果
                         if (err){
                             console.log('喜歡更新失敗');
                         }else{						
-                            console.log('喜歡更新成功' + count); 
+                            console.log('喜歡更新成功'); 
                         }
 
                         //關閉連線
@@ -60,7 +59,7 @@ var bot = linebot({
                         if (err){
                             console.log('不喜歡更新失敗');
                         }else{						
-                            console.log('不喜歡更新成功'+ count); 
+                            console.log('不喜歡更新成功'); 
                         }
 
                         //關閉連線

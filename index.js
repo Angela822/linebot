f61c6a986d047fbc84054f7a82b1aaacf607a770
takebook <takebook107511@gmail.com>
2018-10-20 01:14:50 +0800

    error test

--- index.js
+++ index.js
@@ -1632,7 +1632,7 @@ bot.on('message',function(event) {
                     
                     //查詢資料
                     //(資料庫欄位名稱不使用駝峰命名, 否則可能出錯)
-                        client.query("SELECT book.bookname, book.type, book.picture, userhabit.userid, userhabit.type, userhabit.count FROM book, userhabit where book.type=userhabit.type AND userid= $1 order by count DESC", [userId], (err, results) => {    
+                        client.query("SELECT * from book order by random()", (err, results) => {    
                             console.log(err);
                             
                             //回覆查詢結果	

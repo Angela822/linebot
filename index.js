fe39ad0614b74ca8f53e1bf9f8e0af2855856b15
takebook <takebook107511@gmail.com>
2018-10-20 15:11:12 +0800

    機器人推薦書-test

--- index.js
+++ index.js
@@ -1632,8 +1632,8 @@ bot.on('message',function(event) {
                     
                     //查詢資料
                     //(資料庫欄位名稱不使用駝峰命名, 否則可能出錯)
-                        client.query("SELECT book.bookname, book.type, book.picture, userhabit.type FROM book, userhabit where book.type = userhabit.type AND userid= $1 order by count DESC",[userId],(err, results) => {    
-                            
+                        client.query("SELECT bookname, type, picture FROM book where type='生活風格'",[userId],(err, results) => {    
+                            console.log(err);
                             
                             //回覆查詢結果	
                             var bookname=results.rows[0].bookname;

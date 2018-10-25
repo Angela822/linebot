50f4be78691d3a2ec28c6559a1244773474eac14
unknown <jessie880405@gmail.com>
2018-10-25 14:11:44 +0800

    takebook網址

--- index.js
+++ index.js
@@ -517,7 +517,7 @@ bot.on('message',function(event) {
                                                                     "defaultAction": {
                                                                         "type": "uri",
                                                                         "label": "View detail",
-                                                                        "uri": "http://140.131.114.176/discuss3?" + bookname
+                                                                        "uri": "http://140.131.114.176/"
                                                                     },
                                                                     "actions": [
                                                                         {
@@ -3877,7 +3877,7 @@ bot.on('message',function(event) {
                                                                 "defaultAction": {
                                                                     "type": "uri",
                                                                     "label": "View detail",
-                                                                    "uri": "http://140.131.114.176/"
+                                                                    "uri": "http://140.131.114.176/discuss3?" + bookname
                                                                 },
                                                                 "actions": [
                                                                     {
@@ -4016,7 +4016,7 @@ bot.on('message',function(event) {
                                                                 "defaultAction": {
                                                                     "type": "uri",
                                                                     "label": "View detail",
-                                                                    "uri": "http://140.131.114.176/"
+                                                                    "http://140.131.114.176/discuss3?" + bookname
                                                                 },
                                                                 "actions": [
                                                                     {
@@ -4150,7 +4150,7 @@ bot.on('message',function(event) {
                                                                         "defaultAction": {
                                                                             "type": "uri",
                                                                             "label": "View detail",
-                                                                            "uri": "http://140.131.114.176/"
+                                                                            "http://140.131.114.176/discuss3?" + bookname
                                                                         },
                                                                         "actions": [
                                                                             {

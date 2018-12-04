9d0da515cd6f4d36799a0f27ee44788caf056742
unknown <jessie880405@gmail.com>
2018-12-04 14:36:32 +0800

    chochain

--- index.js
+++ index.js
@@ -4732,12 +4732,13 @@ bot.on('message',function(event) {
                             
                             for(var i = 0;i<results.rows.length;i++){      
                                 var c = results.rows[i].content;
-                                if(c == ''){
-                                    c = '沒有註解喔~~';
+                                if(c != ''){
+                                   array[i]= "\n＊"+results.rows[i].title + "  " + c;
+                                    console.log('進來');
+                                }else{
+                                     c = '沒有註解喔~~';
                                     console.log('進來了?'+c);
-                                }   
-                                array[i]= "\n＊"+results.rows[i].title + "  " + c;
-                                console.log('進來');
+                                }                                
                             }
                             return event.reply([
                                 {

ae0c3988a690f414b6a221a7a1f8c9a8480a9a5b
unknown <jessie880405@gmail.com>
2018-12-07 17:39:53 +0800

    update i like it

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ bot.on('postback', function(event) {
 
                 //--------------like-----------------
                 if(event.postback.data.substring(0,3) == '我喜歡'){
-                    client.query("select * from userhabits a, type b  where a.typeno = b.typeno AND b.typeno = $1 AND a.userid = $2 ",[typeno,userId] ,(err, results) =>{
+                    client.query("select * from userhabits a, type b  where a.typeno = b.typeno AND a.typeno = $1 AND a.userid = $2 ",[typeno,userId] ,(err, results) =>{
                         if(err || results.rows.length==0){
                             console.log(results);
                             client.query("insert into userhabits(userid,typeno,count)values($1,$2,101)",[userId,typeno], (err, results) =>{
